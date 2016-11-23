9432f4780876620bbde9759cc62e5990d50483d1
s290305915 <290305915@qq.com>
2016-11-23 10:52:24 +0800

    Add files via upload

---
+++ Core/DataMapAttribute.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SucLib.Core
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method |
+        AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+    public class DataMapAttribute : Attribute
+    {
+        private string tableName;
+        /// <summary>
+        /// 实体实际对应的表名
+        /// </summary>
+        public string TableName
+        {
+            get { return tableName; }
+            set { tableName = value; }
+        }
+
+        private string columnName;
+        /// <summary>
+        /// 中文列名
+        /// </summary>
+        public string Column
+        {
+            get { return columnName; }
+            set { columnName = value; }
+        }
+        private string belongsto;
+        /// <summary>
+        /// 多对应关系(表)
+        /// </summary>
+        public string BelongsTo
+        {
+            get { return belongsto; }
+            set { belongsto = value; }
+        }
+        private string belongstokey;
+        /// <summary>
+        /// 对应表的ID啊标识之类的
+        /// </summary>
+        public string BelongsToKey
+        {
+            get { return belongstokey; }
+            set { belongstokey = value; }
+        }
+        private string hasmany;
+        /// <summary>
+        /// 表示有一对多的关系 list<T>
+        /// </summary>
+        public string HasMany
+        {
+            get { return hasmany; }
+            set { hasmany = value; }
+        }
+
+        private string targetkey;
+        /// <summary>
+        /// 对应表的标识
+        /// </summary>
+        public string TargetKey
+        {
+            get { return targetkey; }
+            set { targetkey = value; }
+        }
+    }
+}
---
+++ Core/SUC_ROLE.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using SucLib.Core;
+using SucLib.Data.Factory;
+using SucLib.Data.IDal;
+namespace SucLib.Model
+{
+    /// <summary>
+    /// SUC_ROLE:实体类(属性说明自动提取数据库字段的描述信息)
+    /// </summary>
+    [Serializable]
+    [DataMap(TableName = "SUC_ROLE")]
+    public partial class SUC_ROLE : DataBase<SUC_ROLE>
+    {
+        public SUC_ROLE()
+        { }
+        #region Model
+        private int _id;
+        private string _name;
+        private string _remark;
+        private int? _type;
+        private int? _level;
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMap(Column = "ID")]
+        public int ID
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMap(Column = "NAME")]
+        public string NAME
+        {
+            set { _name = value; }
+            get { return _name; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMap(Column = "REMARK")]
+        public string REMARK
+        {
+            set { _remark = value; }
+            get { return _remark; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMap(Column = "TYPE")]
+        public int? TYPE
+        {
+            set { _type = value; }
+            get { return _type; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMap(Column = "LEVEL")]
+        public int? LEVEL
+        {
+            set { _level = value; }
+            get { return _level; }
+        }
+        #endregion Model
+        IDBHelp db = DBFactory.Create(); //实例化工厂
+
+        public IList<SUC_ROLE> Find(string Sql, params object[] args)
+        {
+            return Find(string.Format(Sql, args));
+        }
+        public IList<SUC_ROLE> Find(string Sql)
+        {
+            Sql = string.IsNullOrEmpty(Sql) ? "" : " AND " + Sql;
+            DataTable dt = db.GetDataTable("SELECT * FROM SUC_ROLE WHERE 1=1 " + Sql);
+            return EntityModel.ConvertTo<SUC_ROLE>(dt);
+        }
+    }
+}
+
