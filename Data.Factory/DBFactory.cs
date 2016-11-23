fbc72b40563489d90de2578eb84fd591fe42767f
s290305915 <290305915@qq.com>
2016-11-23 10:55:12 +0800

    Add files via upload

---
+++ Data.Factory/DBFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Reflection;
+using System.Text;
+using SucLib.Data.IDal;
+using SucLib.Common;
+
+namespace SucLib.Data.Factory
+{
+    public class DBFactory
+    {
+        public static IDBHelp Create()
+        {
+            string typeName = "";
+            string text = "SucLib";
+            //string a = ConfigurationSettings.AppSettings["DBType"];
+            string a = "SqlServer"; //ConfigUtil.ConfigHelper.GetConfigString("DBType");
+            if (a == "Access")
+            {
+                typeName = text + ".Data.Dal.AccHelp";
+            }
+            else if (a == "SqlServer" || string.IsNullOrEmpty(a))
+            {
+                typeName = text + ".Data.Dal.SQLHelp";
+            }
+            else if (a == "MySql")
+            { }
+            else if (a == "DB2")
+            { }
+            else if (a == "Oracle")
+            { }
+            else if (a == "SqlLite")
+            { }
+            return (IDBHelp)Assembly.Load(text).CreateInstance(typeName);
+        }
+    }
+}
